9f2fc6be6e1c521ceb106f07bb6c49811dfcbad9
derek <derek@derek-Inspiron-7347>
2016-09-30 11:56:56 -0700

    added rout rule for /dunno using a callback function

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-
+$route['dunno'] = function() {
+    $source = './pix/Anonymous-150x150.jpg'; // an image you provide
+    // set the mime type for that image
+    header("Content-type: image/jpeg");
+    header('Content-Disposition: inline');
+    readfile($source); // dish it
+    die(); // and we don't have to go any further
+}; 
 $route['comp([0-9]+)/([a-zA-Z]+)'] = 'wise/bingo';
 $route['sleep'] = 'first/zzz';
 $route['show/(:num)'] = 'first/gimme/$1';
